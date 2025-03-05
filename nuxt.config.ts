301259ca86cf91e867bb54104899d5cc5f738962
Stefano Rosanelli <stefano.rosanelli@gmail.com>
2025-03-05 12:00:44 +0100

    refactor: restore simpler config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,29 +3,20 @@ export default defineNuxtConfig({
   runtimeConfig: {
     apiSecret: '',
     apiBaseUrl: '',
-    // multi-project configuration
-    projects: {},
     public: {
-      integration: 'brevia', // possibile values: brevia, bedita
       demo: {
         maxChatMessages: '99999', // chatbot msg number per day -> overridden by app
       },
     },
     // brevia integration session & auth credentials
-    brevia: {
-      session: {
-        name: '',
-        secret: '',
-      },
+    session: {
+      name: '',
+      secret: '',
     },
   },
   devtools: { enabled: process.env?.DEVTOOLS === 'true' ? true : false },
   imports: {
     autoImport: true,
-    dirs: ['server'],
   },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', 'nuxt-icon'],
-  $meta: {
-    name: 'nuxt-chatbot-layer',
-  },
 });
